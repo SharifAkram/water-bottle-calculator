06b3e7102777153fb23128c01676d114fff1651e
Sharif Akram <rifkit@gmail.com>
2023-08-05 13:44:18 -0400

    add how_much_water.rb program

---
+++ how_much_water.rb
@@ -0,0 +1,59 @@
+# A calculator to determine how many bottles of water to drink based
+# on your weight in lbs or kg
+
+class WaterBottleCalculator
+      def calculate_bottles(weight, unit)
+            weight = Float(weight)
+            bottles = 0
+
+            if unit.downcase == 'lbs'
+                  # converts lbs into bottles of water
+                  bottles = (weight / 2) / 16.9
+            elsif unit.downcase == 'kg'
+                  # converts kg into bottles of water
+                  bottles = ((weight * 2.20462) / 2) / 16.9
+            end
+
+            bottles.ceil
+      end
+end
+
+class Main
+      def self.run
+            calculator = WaterBottleCalculator.new
+
+            # program will loop until a float is inputted
+            weight = nil
+            until weight.is_a?(Float)
+                  puts "Enter your body weight:"
+                  input = gets.chomp
+
+                  begin
+                        weight = Float(input)
+                  rescue ArgumentError
+                        puts "Invalid input. Please enter a valid number for \
+weight."
+                  end
+            end
+
+            # program will loop until lbs or kg is inputted
+            unit = nil
+            until unit == 'lbs' || unit == 'kg'
+                  puts "Enter the unit (lbs or kg):"
+                  unit = gets.chomp.downcase
+            end
+
+            begin
+                  bottles = calculator.calculate_bottles(weight, unit)
+                  # return value does not have .0
+                  formatted_weight = weight.to_i == weight ? weight.to_i : weight
+                  puts "You are #{formatted_weight} #{unit}. It is \
+recommended that you drink #{bottles} bottles of water \
+per day."
+            rescue => e
+                  puts e.message
+            end
+      end
+end
+
+Main.run
